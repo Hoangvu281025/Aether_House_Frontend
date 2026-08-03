d845206dd1d62f539ce13980000593a9806a1afa
Hoàng Vũ <phamhoangvu281025@gmail.com>
2026-08-03 09:48:02 +0700

    Fix Footer import case

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 // layouts/MainLayout.jsx
 import Header from "../components/Header/header";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/Footer/footer";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
 function MainLayout() {
